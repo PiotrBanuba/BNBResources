495d83ee1b2c6ec8250a462d2a28c0c717cb6632
Piotr Kulbaka <petr.kulbaka@banuba.com>
2023-10-12 14:56:21 +0300

    1.8.0-1

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let bnbPackageVersion: Version = "1.8.0-1"
+
+let package = Package(
+    name: "BNBResources",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "BNBResources",
+            targets: ["BNBResources"]
+        ),
+    ],
+    dependencies: [
+
+    ],
+    targets: [
+        .binaryTarget(
+            name: "BNBResources",
+            url: "https://d2cm7wtcqqw29.cloudfront.net/1.8.0-58-g46079e7d74/BNBResources.zip",
+            checksum: "41dcf2628d49974646c759cefabcacb08c3704c8f34742642b8bf6feb376a8f8"
+        ),
+    ]
+)

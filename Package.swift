861e31cee14e61c40ff994567179a5e326ac98e7
Piotr Kulbaka <petr.kulbaka@banuba.com>
2023-10-18 15:18:01 +0300

    1.8.0-63-g6aa1953f1a

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let bnbPackageVersion: Version = "1.8.0-63-g5825a46ccf"
+let bnbPackageVersion: Version = "1.8.0-63-g6aa1953f1a"
 
 let package = Package(
     name: "BNBResources",
@@ -22,8 +22,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "BNBResources",
-            url: "https://d2cm7wtcqqw29.cloudfront.net/1.8.0-63-g5825a46ccf/BNBResources.zip",
-            checksum: "c82f5553de94b4abec45a0195dbfc41d3a6a4654129088739b1cb847ae3a9c95"
+            url: "https://d2cm7wtcqqw29.cloudfront.net/1.8.0-63-g6aa1953f1a/BNBResources.zip",
+            checksum: "0b98bcc8c2279e2c84a4f5e54246b9c67baf7f9c448dd46ec7713068a9316d45"
         ),
     ]
 )
